446ad860c8929c3fbb9e0099c95bc5f820f03bca
Anton Todorov <antontodorov321@gmail.com>
2024-07-12 16:30:25 +0300

    Add react bootstrap

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import { Header } from "./components/header/Header";
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+    return (
+        <>
+            <Header></Header>
+        </>
+    )
+}
+
+export default App
