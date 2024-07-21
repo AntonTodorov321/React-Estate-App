c80f2b3ced7a1a2b72f7aaad45878822d5dff3aa
Anton Todorov <antontodorov321@gmail.com>
2024-07-21 16:44:18 +0300

    Change favicon

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -2,6 +2,6 @@ import styles from './Footer.module.css';
 
 export default function Footer() {
     return (
-        <footer className={styles.footer}>&copy; 2013-2024 - Real Estate App - All Rights Reserved</footer>
+        <footer className={styles.footer}>&copy; 2024-{new Date().getFullYear()} - Real Estate App - All Rights Reserved</footer>
     );
 };
