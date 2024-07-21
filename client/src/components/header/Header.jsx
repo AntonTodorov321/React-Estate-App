84ba2a6417bf4b6de03cd94139aed7a0b6413549
Anton Todorov <antontodorov321@gmail.com>
2024-07-21 16:49:33 +0300

    Format image into navigation

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,5 +1,7 @@
 import { Link } from 'react-router-dom';
 
+import styles from './Header.module.css';
+
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -8,11 +10,12 @@ export default function Header() {
     return (
         <Navbar expand="lg" className="bg-body-tertiary">
             <Container >
-                <Navbar.Brand as={Link} to='/' href="#home">Real Estate</Navbar.Brand>
+                <img className={styles.img}src="../../public/favicon.png" alt="favicon.png" />
+                <Navbar.Brand as={Link} to='/'>Real Estate</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
-                        <Nav.Link as={Link} to='/all-estates' href="#sss">All Estates</Nav.Link>
+                        <Nav.Link as={Link} to='/all-estates'>All Estates</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
