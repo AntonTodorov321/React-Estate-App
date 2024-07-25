6afa785ef3dbe76ab00e8fb0e5444be53ed3a01a
Anton Todorov <antontodorov321@gmail.com>
2024-07-25 22:55:53 +0300

    Add estate details styles

--- client/src/components/estate-details/EstateDetails.jsx
+++ client/src/components/estate-details/EstateDetails.jsx
@@ -1,9 +1,11 @@
 const baseUrl = 'http://localhost:3030/data/estates';
 
-import { getCurrencySymbol } from '../../utils/currencyUtils.js';
-
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+
+import { getCurrencySymbol } from '../../utils/currencyUtils.js';
+import styles from './EstateDetails.module.css';
+
 
 export default function EstateDetails() {
     const { estateId } = useParams();
@@ -20,17 +22,26 @@ export default function EstateDetails() {
     let currencySymbol = getCurrencySymbol(estate.currency);
 
     return (
-        <div>
-            <h2>{estate.name}</h2>
-            <div>{estate.location}</div>
-            <div>{estate.price} {currencySymbol}</div>
-            <div>({(estate.price / estate.size).toFixed(2)} {currencySymbol}/m<sup>2</sup>)</div>
+        <>
+            <div className={styles.container}>
+                <div className={styles.content}>
+                    <div className={styles.caption}>
+                        <h2>{estate.name}</h2>
+                        <h4>{estate.location}</h4>
+                    </div>
+
+                    <div className={styles.captionDescription}>
+                        <div className={styles.importantRed}>{estate.price} {currencySymbol}</div>
+                        <div>({(estate.price / estate.size).toFixed(2)} {currencySymbol}/m<sup>2</sup>)</div>
+                    </div>
+                </div>
+            </div>
             <h3>{estate.name}</h3>
             <div>{estate.location} ({Math.trunc(estate.price / estate.size)} {currencySymbol}/m<sup>2</sup>)</div>
             <div>Size: {estate.size}m<sup>2</sup>&nbsp; | &nbsp; Floor: {estate.floor}-th from {estate.totalFloors} &nbsp; | &nbsp; Heating: {estate.heating}</div>
             <div>Type of building: {estate.typeOfBuilding}</div>
             <div>Description: {estate.description}</div>
             <div>Contacts: {estate.contacts}</div>
-        </div>
+        </>
     );
 };
