516cda940abe89b3acb020275f21e94d549908a5
Anton Todorov <antontodorov321@gmail.com>
2024-08-05 21:43:14 +0300

    Refactor add estate form

--- client/src/components/add-estate/AddEstate.jsx
+++ client/src/components/add-estate/AddEstate.jsx
@@ -1,56 +1,108 @@
+const FORM_KEYS = {
+    name: 'name',
+    contacts: 'contacts',
+    description: 'description',
+    floor: 'floor',
+    totalFloors: 'total-floors',
+    price: 'price',
+    size: 'size'
+};
+
+console.log(FORM_KEYS);
+
 export default function AddEstate() {
+
     return (
-        <div>
-            <label htmlFor="name">Name: </label>
-            <input type="text" id="name"></input>
-            <br />
-            <label htmlFor="contacts">Contacts: </label>
-            <input type="text" id="contacts"></input>
-            <br />
-            <label htmlFor="description">Description: </label>
-            <input type="text" id="description"></input>
-            <br />
-            <label htmlFor="floor">Floor: </label>
-            <input type="number" id="floor"></input>
-            <label>from: </label>
-            <input type="number"></input>
-            <br />
-            <label>Heating: </label>
-            <select>
-                <option value=""></option>
-                <option value="option1">GAS</option>
-                <option value="option1">Air conditioner</option>
-                <option value="option1">TPP</option>
-            </select>
-            <br />
-            <label>Location: </label>
-            <select>
-                <option value=""></option>
-                <option value="option1">Manastirski Livadi</option>
-                <option value="option1">Lozenec</option>
-                <option value="option1">Iztok</option>
-                <option value="option1">Studentski Grad</option>
-                <option value="option1">Mladost</option>
-            </select>
-            <br />
-            <label htmlFor="price">Price: </label>
-            <input type="number" id="price"></input>
-            <select>
-                <option value=""></option>
-                <option value="option1">lv</option>
-                <option value="option2">EUR</option>
-            </select>
-            <br />
-            <label htmlFor="size">Size: </label>
-            <input type="number" id="size"></input>
-            <br />
-            <label>Type of building: </label>
-            <select>
-                <option value=""></option>
-                <option value="option1">brick</option>
-                <option value="option1">EPK</option>
-                <option value="option1">panel</option>
-            </select>
-        </div>
+        <>
+            <form>
+                <label htmlFor="name">Name: </label>
+                <input
+                    type="text"
+                    name={FORM_KEYS.name}
+                    id={FORM_KEYS.name}
+                />
+                <br />
+
+                <label htmlFor="contacts">Contacts: </label>
+                <input
+                    type="text"
+                    name="contacts"
+                    id="contacts"
+                />
+                <br />
+
+                <label htmlFor="description">Description: </label>
+                <input
+                    type="text"
+                    name="description"
+                    id="description"
+                />
+                <br />
+
+                <label htmlFor="floor">Floor: </label>
+                <input
+                    type="number"
+                    name="floor"
+                    id="floor"
+                />
+
+                <label>from: </label>
+                <input
+                    type="number"
+                    name="total-floors"
+                />
+                <br />
+
+                <label>Heating: </label>
+                <select>
+                    <option value=""></option>
+                    <option value="GAS">GAS</option>
+                    <option value="Air conditioner">Air conditioner</option>
+                    <option value="TPP">TPP</option>
+                </select>
+                <br />
+
+                <label>Location: </label>
+                <select>
+                    <option value=""></option>
+                    <option value="Manastirski Livadi">Manastirski Livadi</option>
+                    <option value="Lozenec">Lozenec</option>
+                    <option value="Iztok">Iztok</option>
+                    <option value="Studentski Grad">Studentski Grad</option>
+                    <option value="Mladost">Mladost</option>
+                </select>
+                <br />
+
+                <label htmlFor="price">Price: </label>
+                <input
+                    type="number"
+                    name="price"
+                    id="price"
+                />
+                <select>
+                    <option value=""></option>
+                    <option value="lv">lv</option>
+                    <option value="EUR">EUR</option>
+                </select>
+                <br />
+
+                <label htmlFor="size">Size: </label>
+                <input
+                    type="number"
+                    name="size"
+                    id="size"
+                />
+                <br />
+
+                <label>Type of building: </label>
+                <select>
+                    <option value=""></option>
+                    <option value="brick">brick</option>
+                    <option value="EPK">EPK</option>
+                    <option value="panel">panel</option>
+                </select>
+                <button type="button">Submit</button>
+            </form>
+        </>
     )
 }
