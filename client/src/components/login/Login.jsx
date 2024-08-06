dd285c67fd542039e84d29fdd6e69f95836b0381
Anton Todorov <antontodorov321@gmail.com>
2024-08-06 21:27:56 +0300

    Make login form controlled

--- client/src/components/login/Login.jsx
+++ client/src/components/login/Login.jsx
@@ -1,20 +1,52 @@
+import { useState } from 'react';
+
 import styles from './Login.module.css';
 
 export default function () {
+    const [formValues, setFormValues] = useState({
+        username: '',
+        password: ''
+    });
+
+    const changeHandler = (e) => {
+        setFormValues(state => ({
+            ...state,
+            [e.target.name]: e.target.value
+        }))
+    };
+
+    const submitHandler = () => {
+        console.log(formValues);
+    };
+
     return (
         <div className={styles.container}>
             <div className={styles.loginBox}>
                 <h2 className={styles.title}>Login</h2>
-                <form>   
+                <form>
                     <div className={styles.inputGroup}>
                         <label htmlFor="username">Username:</label>
-                        <input type="text" id="username" name="username" required className={styles.input} />
+                        <input
+                            type="text"
+                            name="username"
+                            className={styles.input}
+                            onChange={changeHandler}
+                            value={formValues.username}
+                        />
                     </div>
+
                     <div className={styles.inputGroup}>
                         <label htmlFor="password">Password:</label>
-                        <input type="password" id="password" name="password" required className={styles.input} />
+                        <input
+                            type="password"
+                            name="password"
+                            className={styles.input}
+                            onChange={changeHandler}
+                            value={formValues.password}
+                        />
                     </div>
-                    <button type="submit" className={styles.button}>Login</button>
+
+                    <button type="button" onClick={submitHandler} className={styles.button}>Login</button>
                 </form>
             </div>
         </div>
