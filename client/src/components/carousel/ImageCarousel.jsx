718b47b76d3c4e9f33c4746c11d23fc89c4ff678
Anton Todorov <antontodorov321@gmail.com>
2024-08-16 23:37:39 +0300

    User can change pictures with arrows

--- client/src/components/carousel/ImageCarousel.jsx
+++ client/src/components/carousel/ImageCarousel.jsx
@@ -1,17 +1,41 @@
+import { useEffect, useRef } from 'react';
+
 import { Carousel } from 'react-responsive-carousel';
 
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 export default function ImageCarousel({
-    allImg
+    allImg,
 }) {
+    const focusDivRef = useRef();
+    const carouselRef = useRef();
+
+    useEffect(() => {
+        focusDivRef.current.focus();
+    }, []);
+
+    const handleKeyDown = (e) => {
+        if (e.key === 'ArrowLeft') {
+            carouselRef.current.decrement();
+        } else if (e.key === 'ArrowRight') {
+            carouselRef.current.increment();
+        }
+    };
+
     return (
-        <Carousel showThumbs={false} infiniteLoop={true} autoPlay={true}>
-            {allImg.map((img) => (
-                <div key={img}>
-                    <img src={img}  />
-                </div>
-            ))}
-        </Carousel>
+        <div ref={focusDivRef} onKeyDown={handleKeyDown} tabIndex='0'>
+            <Carousel
+                showThumbs={true}
+                autoPlay={false}
+                infiniteLoop={true}
+                ref={carouselRef}
+            >
+                {allImg.map((img) => (
+                    <div key={img}>
+                        <img src={img} />
+                    </div>
+                ))}
+            </Carousel>
+        </div>
     );
 }
