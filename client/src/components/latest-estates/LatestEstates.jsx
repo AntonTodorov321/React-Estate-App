3461bf0e3b514ff72dff189f599d184532a9869b
Anton Todorov <antontodorov321@gmail.com>
2024-09-11 23:14:18 +0300

    Add hover effects to estate images on home page

--- client/src/components/latest-estates/LatestEstates.jsx
+++ client/src/components/latest-estates/LatestEstates.jsx
@@ -4,25 +4,37 @@ import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
 import styles from './LatestEstates.module.css';
+import { getCurrencySymbol } from '../../utils/estateUtils';
 
 export default function LatestEstates({
-    _id,    
+    _id,
     typeOfEstate,
     mainImg,
-    description
+    description,
+    location,
+    price,
+    size,
+    currency
 }) {
     return (
         <Card className={styles.card}>
-            <Card.Img variant="top" src={mainImg} alt={typeOfEstate} className={styles.img} />
+            <div className={styles.imgWrapper}>
+                <Card.Img variant="top" src={mainImg} alt={typeOfEstate} className={styles.img} />
+                <div className={styles.moreInfo}>
+                    <p>Location: {location}</p>
+                    <p>Price: {price} {getCurrencySymbol(currency)}</p>
+                    <p>Size: {size} m<sup>2</sup></p>
+                </div>
+            </div>
             <Card.Body className={styles.cardBody}>
                 <Card.Title className={styles.textCenter}>Rent {typeOfEstate} apartment</Card.Title>
                 <Card.Text className={styles.textCenter}>
                     {description}
                 </Card.Text>
             </Card.Body>
-                <div className={styles.textCenter}>
-                    <Button as={Link} to={`/estates/${_id}`} variant="primary" className={styles.button}>Details</Button>
-                </div>
+            <div className={styles.textCenter}>
+                <Button as={Link} to={`/estates/${_id}`} variant="primary" className={styles.button}>Details</Button>
+            </div>
         </Card>
     );
 };
