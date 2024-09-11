c988982202285a8a2862d6b290f529352272dcb8
Anton Todorov <antontodorov321@gmail.com>
2024-09-11 22:14:29 +0300

    Add more styling to home page

--- client/src/components/latest-estates/LatestEstates.jsx
+++ client/src/components/latest-estates/LatestEstates.jsx
@@ -5,24 +5,24 @@ import Button from 'react-bootstrap/Button';
 
 import styles from './LatestEstates.module.css';
 
-export default function GetLatest({
-    _id,
+export default function LatestEstates({
+    _id,    
     typeOfEstate,
     mainImg,
     description
 }) {
     return (
         <Card className={styles.card}>
-            <Card.Img variant="top" src={mainImg} alt={typeOfEstate} className={styles.Img} />
+            <Card.Img variant="top" src={mainImg} alt={typeOfEstate} className={styles.img} />
             <Card.Body className={styles.cardBody}>
                 <Card.Title className={styles.textCenter}>Rent {typeOfEstate} apartment</Card.Title>
                 <Card.Text className={styles.textCenter}>
                     {description}
                 </Card.Text>
+            </Card.Body>
                 <div className={styles.textCenter}>
                     <Button as={Link} to={`/estates/${_id}`} variant="primary" className={styles.button}>Details</Button>
                 </div>
-            </Card.Body>
         </Card>
     );
 };
